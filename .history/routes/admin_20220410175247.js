3ac44d3414f2ee8983f52dbb6f62aef170d0b01d
claytoneduard <clayton_eduard@hotmail.com>
2022-04-10 19:47:38 -0300

    Cadastro de Usuario finalizado

---
+++ .history/routes/admin_20220410175247.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const { route } = require("express/lib/application");
+const req = require("express/lib/request");
+//componente para criar rotas separadas
+const router = express.Router();
+//carregando o mongo
+const mongoose = require("mongoose");
+//carregando os dados dos usuários
+//const Usuario = mongoose.model("usuario");
+
+//definindo as rotas
+router.get("/", (req, res) => {
+    res.send("Pagina principal do painel ADM");
+    res.render("admin/index");
+});
+
+router.get("/usuarios", (req, res) => {
+    res.send("Pagina principal do painel ADM");
+    res.render("admin/usuarios");
+});
+
+//exporta o modulo
+module.exports = router;
---
+++ .history/routes/admin_20220410185929.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { route } = require("express/lib/application");
+const req = require("express/lib/request");
+//componente para criar rotas separadas
+const router = express.Router();
+//carregando o mongo
+const mongoose = require("mongoose");
+//carregando os dados dos usuários
+const Usuario = mongoose.model("usuarios");
+require("../models/Usuario")
+    //definindo as rotas
+router.get("/admin", (req, res) => {
+    res.send("Pagina principal do painel ADM");
+    res.render("admin/index");
+});
+
+
+
+//exporta o modulo
+module.exports = router;
